d0e8754dca7a66de2b0bfa4471c6bd9d9904ff63
u9g <43508353+u9g@users.noreply.github.com>
2022-08-24 22:49:53 -0400

    some work

---
+++ client_utilities.js
@@ -0,0 +1,15 @@
+module.exports = {
+    sendChat(toClient, jsonMsg) {
+        toClient.write('chat', { message: jsonMsg, position: 0 })
+    },
+    sendBigTitle(toClient, jsonMsg) {
+        toClient.write('title', { action: 0, text: jsonMsg })
+    },
+    sendManyChat(toClient, jsonMsg) {
+        for(let i=0;i<5;i++)this.sendChat(toClient, jsonMsg)
+    },
+    sendBigTitleAndManyChat(toClient, jsonMsg) {
+        this.sendBigTitle(toClient, jsonMsg)
+        this.sendManyChat(toClient, jsonMsg)
+    }
+}
--- index.js
+++ index.js
@@ -1,76 +1,153 @@
 const { InstantConnectProxy } = require('prismarine-proxy')
 const pnbt = require('prismarine-nbt')
-const mojangson = require('mojangson')
+const mcdata = require('minecraft-data')('1.8.9')
+const constants = require('./constants')
+const { moneyize } = constants
+const clientUtils = require('./client_utilities')
 
 const pchat = require('prismarine-chat')('1.8.9')
+const { onceWithCleanup } = require('mineflayer/lib/promise_utils')
+
+const { fetch } = require('undici')
+
+const config = {
+  remake_item_lore: true,
+  exec_extender_ah_min_price: true,
+  disable_powerball: true,
+  disable_items_on_ground: true,
+  show_pets_on_cooldown: true,
+  exec_extender_trade_price: true,
+  show_full_midas_satchels: true
+}
 
 const proxy = new InstantConnectProxy({
   loginHandler: (client) => {
-    console.log('logging in with: ' + client.username)
-    if (client.username === 'U9GBot' || client.username === '4h1') return { username: client.username+'1', auth: 'microsoft' }
+    if (client.username === 'U9GBot' || client.username === '4h1' || client.username === 'PullRequest') return { username: client.username + '2', auth: 'microsoft' }
     return { username: client.username, auth: 'microsoft' }
   },
-  serverOptions: { 
+  serverOptions: {
     version: '1.8.9',
     fakeHost: 'cosmicprisons.com',
     port: 25566
   },
-  clientOptions: { 
+  clientOptions: {
     version: '1.8.9',
-    host: 'cosmicprisons.com' 
+    host: 'cosmicprisons.com'
   }
 })
 
-const wait = require('util').promisify(setTimeout)
+let windowId = 0
 
-const one = {
-  billion: 1.0e+9,
-  million: 1.0e+6,
-  thousand: 1.0e+3
+const trade = {
+  inTrade: false,
+  tradeData: null
 }
 
-function moneyize (v) {
-  if (v >= one.billion)
-    return (v / one.billion).toFixed(0) + 'b'
-  else if (v >= one.million)
-    return (v / one.million).toFixed(0) + 'm'
-  else if (v >= one.thousand)
-    return (v / one.thousand).toFixed(0) + 'k'
-  return v
+const activeBuffs = {
+  cauldron: false,
+  hastePet: false
 }
+
+const timeouts = {}
+
+proxy.on('start', () => {
+  trade.inTrade = false
+  trade.tradeData = null
+})
+
+proxy.on('end', () => {
+  trade.inTrade = false
+  trade.tradeData = null
+})
 
 proxy.on('incoming', async (data, meta, toClient, toServer) => {
   if (meta.name === 'world_particles') return
   if (meta.name === 'chat') {
     const msg = pchat.fromNotch(data.message).toString()
-    if (msg.startsWith("(!) Your Cauldron: 200% /skill booster effect has expired."))  {
-      const m = JSON.stringify({"bold":true,"color":"green","extra":[{"color":"red","text":"OVER"}],"text":"CAULDRON "})
-      toClient.write('title', { action: 0, text: m })
-      for(let i=0;i<5;i++)toClient.write('chat', {message: m, position: 0})
-    } else if (msg.startsWith("(!) Your Mining Speed Booster has expired.")) {
-      const m = JSON.stringify({"bold":true,"color":"aqua","extra":[{"color":"red","text":"OVER"}],"text":"HASTE PET "})
-      toClient.write('title', { action: 0, text: m })
-      for(let i=0;i<5;i++)toClient.write('chat', {message: m, position: 0})
+    if (msg.startsWith(constants.CAULDRON_OVER_MESSAGE)) {
+      activeBuffs.cauldron = false
+      clientUtils.sendBigTitleAndManyChat(toClient, constants.CAULDRON_OVER_MESSAGE_PLAYER_NOTIFICATION)
+    } else if (msg.startsWith(constants.HASTE_PET_OVER_MESSAGE)) {
+      activeBuffs.hastePet = true
+      clientUtils.sendBigTitleAndManyChat(toClient, constants.HASTE_PET_OVER_MESSAGE_PLAYER_NOTIFICATION)
+    } else if (msg === '(!) Welcome to the Executive Mine.') {
+      await onEnterExec()
+    } else if (msg === constants.CAULDRON_ACTIVE_MESSAGE) {
+      activeBuffs.cauldron = true
+    } else if (constants.HASTE_PET_ACTIVE_REGEX.test(msg)) {
+      activeBuffs.hastePet = true
     }
-  }
-  if (meta.name === 'spawn_entity' || meta.name === 'spawn_entity_living') {
+  } else if (meta.name === 'spawn_entity' || meta.name === 'spawn_entity_living') {
     const { type } = data
-    if (type === 30 && meta.name === 'spawn_entity_living' && data.metadata.find(b => b.key === 2)?.value === '') return // ore drop animation
-    if (meta.name === 'spawn_entity' && type === 2) return // powerball
+    if (type === 30 && meta.name === 'spawn_entity_living' && data.metadata.find(b => b.key === 2)?.value === '' && config.disable_powerball) return // powerball
+    else if (type === 2 && meta.name === 'spawn_entity' && config.disable_items_on_ground) return // ore drop animation
+  } else if (meta.name === 'set_slot' && data.item.nbtData) {
+    await handleItem(data.item)
+    if (trade.inTrade) {
+      trade.tradeData[data.slot] = data.item
+    }
+  } else if (meta.name === 'window_items') {
+    let i = 0
+    for (const item of data.items) {
+      await handleItem(item)
+      if (trade.inTrade && data.windowId !== 0) {
+        trade.tradeData[i++] = item
+      }
+    }
+  } else if (meta.name === 'open_window') {
+    trade.inTrade = /§l([a-zA-Z0-9_]+) +([a-zA-Z0-9_]+)/.test(data.windowTitle) // FIX THIS SHOULDNT SHOW UP DURING /itemclaim OR for brags
+    if (!trade.inTrade && trade.tradeData) {
+      trade.tradeData = null
+    } else if (trade.inTrade && !trade.tradeData) {
+      trade.tradeData = {}
+    }
+    windowId = data.windowId
+  } if (meta.name === 'close_window') {
+    trade.inTrade = false
+    trade.tradeData = null
   }
 
-  if (meta.name === 'set_slot' && data.item.nbtData) {
-    handleItem(data.item)
-  } else if (meta.name === 'window_items') {
-    for (const item of data.items) {
-      handleItem(item)
+  if (meta.name === 'world_event' && data.effectId === 2001) return
+  toClient.write(meta.name, data)
+
+  if (config.exec_extender_trade_price && (meta.name === 'set_slot' || meta.name === 'window_items')/* && (trade?.tradeData?.theirSideMins??0) > 0 && (trade?.tradeData?.yourSideMoney??0) > 0 */ && trade.inTrade) {
+    trade.tradeData.theirSideMins = numberOfExecMinsInOppositeSide()
+    trade.tradeData.yourSideMoney = numberOfMoneyInYourSide()
+    if ((trade?.tradeData?.theirSideMins ?? 0) > 0 && (trade?.tradeData?.yourSideMoney ?? 0) > 0) {
+      toClient.write('set_slot', {
+        windowId,
+        slot: 22,
+        item: {
+          blockId: mcdata.blocksByName.pumpkin.id,
+          itemCount: 1,
+          itemDamage: 0,
+          nbtData: {
+            type: 'compound',
+            name: '',
+            value: {
+              display: {
+                type: 'compound',
+                value: {
+                  Name: { type: 'string', value: `§a$${moneyize(trade.tradeData.yourSideMoney / trade.tradeData.theirSideMins)} §8/ min` },
+                  Lore: {
+                    type: 'list',
+                    value: {
+                      type: 'string',
+                      value:
+            [
+              '',
+              `§a$${moneyize(trade.tradeData.yourSideMoney)} §8to §a${trade.tradeData.theirSideMins} mins`
+            ]
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      })
     }
   }
-
-  // if (meta.name === 'chat') {
-  //   data.message = handleChat(data.message)
-  // }
-  toClient.write(meta.name, data) 
 })
 
 // MOJANGSON PARSER CANT HANDLE ITEMS
@@ -101,68 +178,57 @@ proxy.on('incoming', async (data, meta, toClient, toServer) => {
 //   }
 // }
 
-const NOT_MAX_ENCHANT_REGEX = /^§.(.+) §b([IV]+)$/ // need forced start end for timewarp
-const MAX_ENCHANT_REGEX = /^§.§l(.+) §b§l([IV]+)$/ // need forced start end for timewarp
-const NOT_MAX_ENCHANT_WITH_EXTRA_REGEX = /§.(.+) §b([IV]+)(§7 .+)/
-const MAX_ENCHANT_WITH_EXTRA_REGEX = /§.§l(.+) §b§l([IV]+)(§7 .+)/
-const ORE_MINED_AMOUNT_REGEX = /§f[\d,]+ §.§l(?:[a-zA-Z]+) Ore/
-const CC_REQUIRED_REGEX = /§7\(§nCosmicClient.com§7 required\)/
-const CHARGE_ORB_SLOT_LINE = /§f§l§n(\d+)§7\/§f(\d+)§b Charge Orb §7slots unlocked/
-
 const ColorProfile = {
   bright_rainbow: 'c6ea9b5',
   winter: '3bf'
 }
 
-const colorize = (text, colors, settings = { bold: false }) => text.split('').map((letter, ix) => `§${colors[ix % colors.length]}${(settings?.bold??false) ? '§l' : ''}${letter}`).join('')
+const colorize = (text, colors, settings = { bold: false }) => text.split('').map((letter, ix) => `§${colors[ix % colors.length]}${(settings?.bold ?? false) ? '§l' : ''}${letter}`).join('')
 
-function handleItem(item) {
+async function handleItem (item) {
   if (!item.nbtData) return
   const nbt = pnbt.simplify(item.nbtData)
   const lore = item?.nbtData?.value?.display?.value?.Lore?.value?.value
 
-  // console.log(nbt)
-  const priceLine = nbt?.display?.Lore?.find(b => b.toLowerCase().includes('price (ea): §a$'))
-  if (priceLine && nbt._x === 'exectimeextender') {
-    const ix = nbt.display.Lore.indexOf(priceLine)
-    const price = +priceLine.substring(priceLine.includes('Low ') ? 17 + 4 : 17).replace(/,/g, '')
-    const mins = nbt.exectimeextender/60
-    // console.log('changed line')
-    lore[ix] = priceLine + ` §8(§a$${moneyize(price/mins)} §8/ min)`
+  if (config.exec_extender_ah_min_price) {
+    const priceLine = nbt?.display?.Lore?.find(b => b.toLowerCase().includes('price (ea): §a$'))
+    if (priceLine && nbt._x === 'exectimeextender') {
+      const ix = nbt.display.Lore.indexOf(priceLine)
+      const price = +priceLine.substring(priceLine.includes('Low ') ? 17 + 4 : 17).replace(/,/g, '')
+      const mins = nbt.exectimeextender / 60
+      lore[ix] = priceLine + ` §8(§a$${moneyize(price / mins)} §8/ min)`
+    }
   }
 
-  
-  if (Array.isArray(lore)) {
+  if (config.remake_item_lore && Array.isArray(lore)) {
     for (let i = 0; i < lore.length; i++) {
       const line = lore[i]
-      if (MAX_ENCHANT_REGEX.test(line)) {
-        const [,enchName, enchLevel] = line.match(MAX_ENCHANT_REGEX)
-        lore[i] = `§8${enchName} ${enchLevel}`
-      } else if (NOT_MAX_ENCHANT_REGEX.test(line)) {
-        const [,enchName, enchLevel] = line.match(NOT_MAX_ENCHANT_REGEX)
-        lore[i] = `§8${enchName} ${enchLevel}`
-      } else if (NOT_MAX_ENCHANT_WITH_EXTRA_REGEX.test(line)) {
-        const [,enchName, enchLevel, extra] = line.match(NOT_MAX_ENCHANT_WITH_EXTRA_REGEX)
-        lore[i] = `§8${enchName} ${enchLevel}${extra}`
-        // lore[i] = lore[i].replace(/§./g, '§8')
-      } else if (MAX_ENCHANT_WITH_EXTRA_REGEX.test(line)) {
-        const [,enchName, enchLevel, extra] = line.match(MAX_ENCHANT_WITH_EXTRA_REGEX)
-        lore[i] = `${colorize(enchName.replace(/§./g, ''), ColorProfile.winter, {bold: true})} §b§l${enchLevel}${extra}`
-      } /*else if (CHARGE_ORB_SLOT_LINE.test(line))*/
-      // §dEnergy Hoarder §bII
-    }
-    for (let i = 0; i < lore.length; i++) {
-      const line = lore[i]
-      if (lore[i-1] === '' && ORE_MINED_AMOUNT_REGEX.test(line)) { // remove ore mined lines
-        let first = i-1
-        let last = first
-        while (ORE_MINED_AMOUNT_REGEX.test(lore[last + 1])) {
-          last++
-        }
-        lore.splice(first, last-first + 1)
+      if (constants.MAX_ENCHANT_REGEX.test(line)) {
+        const [, enchName, enchLevel] = line.match(constants.MAX_ENCHANT_REGEX)
+        lore[i] = colorizeEnchantment(enchName, enchLevel, '', true)
+      } else if (constants.NOT_MAX_ENCHANT_REGEX.test(line)) {
+        const [, enchName, enchLevel] = line.match(constants.NOT_MAX_ENCHANT_REGEX)
+        lore[i] = colorizeEnchantment(enchName, enchLevel, '', false)
+      } else if (constants.NOT_MAX_ENCHANT_WITH_EXTRA_REGEX.test(line)) {
+        const [, enchName, enchLevel, extra] = line.match(constants.NOT_MAX_ENCHANT_WITH_EXTRA_REGEX)
+        lore[i] = colorizeEnchantment(enchName, enchLevel, extra, false)
+      } else if (constants.MAX_ENCHANT_WITH_EXTRA_REGEX.test(line)) {
+        const [, enchName, enchLevel, extra] = line.match(constants.MAX_ENCHANT_WITH_EXTRA_REGEX)
+        lore[i] = colorizeEnchantment(enchName, enchLevel, extra, true)
       }
     }
-    
+    for (let i = 0; i < lore.length; i++) {
+      const line = lore[i]
+      if (lore[i - 1] === '' && constants.ORE_MINED_AMOUNT_REGEX.test(line)) { // remove ore mined lines
+        const first = i - 1
+        let last = first
+        while (constants.ORE_MINED_AMOUNT_REGEX.test(lore[last + 1])) {
+          last++
+        }
+        lore.splice(first, last - first + 1)
+      }
+    }
+
     // remove gkit story on pickaxe
     const story = nbt?.chargable?.desc?.split('\n')
     if (story?.length > 0) {
@@ -173,18 +239,173 @@ function handleItem(item) {
       }
     }
 
-    if (CC_REQUIRED_REGEX.test(lore.slice(-1))) {
+    if (constants.CC_REQUIRED_REGEX.test(lore.slice(-1))) {
       lore.splice(-1, 1)
     }
 
     for (let i = 0; i < lore.length; i++) {
-      if (!CHARGE_ORB_SLOT_LINE.test(lore[i])) continue
+      if (!constants.CHARGE_ORB_SLOT_LINE.test(lore[i])) continue
       lore.splice(i, 2)
       break
+    }
+
+    for (let i = 0; i < lore.length; i++) {
+      if (lore[i].includes('XP Mastery')) {
+        lore[i] = lore[i].replace(/(§.)/g, '$1§l')
+      }
+    }
+  }
+
+  // console.log(nbt)
+  if (config.show_pets_on_cooldown && (nbt._x === 'pet' || nbt._x === 'trinket') && nbt?.cosmicData?.cooldown) {
+    const canProcAgainAt = +nbt.cosmicData.lastUsed.toString() + +nbt.cosmicData.cooldown.toString()
+    const now = Date.now()
+    if (canProcAgainAt > now) {
+      let diffSec = (canProcAgainAt - now) / 1000
+      const str = []
+      if (diffSec > (60 * 60)) {
+        str.push(`${Math.floor(diffSec / (60 * 60))} hr`)
+        diffSec %= (60 * 60)
+      }
+      if (diffSec > 60) {
+        str.push(`${Math.floor(diffSec / 60)} min`)
+        diffSec %= 60
+      }
+      str.push(`${Math.floor(diffSec)} sec`)
+      item.nbtData.value.display.value.Name.value += ` §8(§f${str.join(' ')}§8)`
+      item.blockId = mcdata.blocksByName.dragon_egg.id
+      item.itemDamage = 0
+    }
+  }
+
+  if (config.show_full_midas_satchels && nbt._x === 'midassatchel' && nbt.__count === 15000) {
+    item.nbtData.value.display.value.Name.value = '§4§lFULL §r' + item.nbtData.value.display.value.Name.value
+    item.blockId = mcdata.blocksByName.gold_block.id
+    item.itemDamage = 0
+  }
+
+  // avg price on lore
+  if (nbt._x === 'whitescroll') {
+    const resp = await (await fetch('http://localhost/sov/whitescroll')).json()
+    if (resp.ok) {
+      lore.push('')
+      lore.push(`§6§lAverage Price: §r§a§l$${moneyize(resp.avgPrice.toFixed(2))} §8(for the last day)`)
+    }
+  } else if (nbt._x === 'holy_whitescroll') {
+    const resp = await (await fetch('http://localhost/sov/holy_whitescroll')).json()
+    if (resp.ok) {
+      lore.push('')
+      lore.push(`§6§lAverage Price: §r§a§l$${moneyize(resp.avgPrice.toFixed(2))} §8(for the last day)`)
+    }
+  } else if (nbt._x === 'blackscroll' && nbt.heroic !== 1) {
+    const resp = await (await fetch(`http://localhost/sov/blackscroll/${nbt['joeBlackScroll-chance']}`)).json()
+    if (resp.ok) {
+      lore.push('')
+      lore.push(`§6§lAverage Price: §r§a§l$${moneyize(resp.avgPrice.toFixed(2))} §8(for the last 3 days)`)
+    }
+  } else if (nbt._x === 'blackscroll' && nbt.heroic === 1) {
+    const resp = await (await fetch(`http://localhost/sov/heroic_blackscroll/${nbt['joeBlackScroll-chance']}`)).json()
+    if (resp.ok) {
+      lore.push('')
+      lore.push(`§6§lAverage Price: §r§a§l$${moneyize(resp.avgPrice.toFixed(2))} §8(for the last 3 days)`)
+    }
+  } else if (nbt._x === 'skin') {
+    const resp = await (await fetch(`http://localhost/sov/skin/${nbt.joe.data.types}`)).json()
+    if (resp.ok) {
+      lore.push('')
+      lore.push(`§6§lAverage Price: §r§a§l$${moneyize(resp.avgPrice.toFixed(2))} §8(for the last 3 days)`)
     }
   }
 }
 
-proxy.on('outgoing', (data, meta, toClient, toServer) => { 
+function colorizeEnchantment (name, level, extraText, isMax) {
+  const nonColoredName = name.replace(/§./g, '')
+  let finalEnchName = name
+  let finalExtraText = extraText
+  const finalLevel = '§b' + (isMax ? '§l' : '') + level
+  if (extraText) {
+    /* || nonColoredName === 'Ore Miner' */
+    if (nonColoredName === 'Time Warp' || nonColoredName === 'Warp Miner') {
+      finalEnchName = colorize(nonColoredName, ColorProfile.bright_rainbow, { bold: true })
+    } else {
+      finalExtraText = ''
+    }
+  }
+
+  return `${finalEnchName} ${finalLevel} ${finalExtraText}`
+}
+
+function numberOfExecMinsInOppositeSide () {
+  if (Object.keys(trade.tradeData) === 0) return 0
+  let mins = 0
+  for (const slotNum of constants.oppositeSideSlots) {
+    const item = trade.tradeData[slotNum]
+    mins += item?.nbtData?.value?.exectimeextender?.value ?? 0
+  }
+  return mins / 60
+}
+
+function numberOfMoneyInYourSide () {
+  if (Object.keys(trade.tradeData) === 0) return 0
+  let money = 0
+  for (const slotNum of constants.ourSide) {
+    const item = trade.tradeData[slotNum]
+    money += parseInt(item?.nbtData?.value?.money?.value ?? '0')
+  }
+  return money
+}
+
+async function onEnterExec () {
+  await onceWithCleanup(proxy, 'incoming', {
+    checkCondition: (data, meta, toClient, toServer) => meta.name === 'window_items'
+  })
+  const [{ items },, toClient] = await onceWithCleanup(proxy, 'incoming', {
+    checkCondition: (data, meta, toClient, toServer) => meta.name === 'window_items'
+  })
+  let hasNaturalXP = false
+  const synthetics = {}
+  for (let i = 5; i <= 8; i++) { // iterate armor
+    if (items[i]?.nbtData?.value?.chargable?.value && items[i]?.nbtData?.value?.chargable?.value?.holyWhiteScroll?.value !== 1) {
+      toClient.write('chat', { position: 0, message: constants.UNHOLIED_MESSAGES[i - 5] })
+    }
+  }
+  for (const item of items) {
+    if (item.blockId !== -1) {
+      const mcdItem = mcdata.items[item.blockId]
+      if (mcdItem.name.endsWith('_pickaxe') && item.nbtData?.value?.chargable?.value?.whiteScroll?.value !== 1) {
+        clientUtils.sendChat(toClient, JSON.stringify({ bold: true, extra: [{ text: item?.nbtData?.value?.display?.value?.Name?.value ?? mcdItem.displayName, extra: [{ color: 'red', text: ' << UNWHITESCROLLED' }] }], text: 'YOU LEFT YOUR PICKAXE CALLED >> ' }))
+      } else if (item?.nbtData?.value?._x?.value === 'miningxp' && !item?.nbtData?.value?.['joe-miningXP-extractor']) {
+        hasNaturalXP = true
+      } else if (item?.nbtData?.value?._x?.value === 'synthetic') {
+        synthetics[item.nbtData.value.__type.value] = true
+      }
+    }
+  }
+
+  if (!hasNaturalXP) clientUtils.sendManyChat(toClient, constants.NO_GODLY_XP_BOTTLE_NOTIFICATION)
+
+  Object.entries(constants.DONT_HAVE_SYNTHETICS_MESSAGES)
+    .filter(([syntheticName]) => !synthetics[syntheticName])
+    .forEach(([, msg]) => clientUtils.sendChat(toClient, msg))
+}
+
+proxy.on('outgoing', (data, meta, toClient, toServer) => {
+  if (meta.name === 'chat') {
+    const { message } = data
+    if (message in constants.MIDAS_CORNERS) {
+      timeouts.midas = setTimeout(() => {
+        clientUtils.sendBigTitleAndManyChat(toClient, constants.midasSpawnedInCorner(message))
+        delete timeouts.midas
+      }, (10 * constants.minutes) - (10 * constants.seconds))
+
+      timeouts.midasTwoMin = setTimeout(() => {
+        clientUtils.sendBigTitleAndManyChat(toClient, constants.midasWillSpawnInCorner(message, '2 Minutes'))
+        delete timeouts.midas
+      }, ((10 * constants.minutes) - (10 * constants.seconds)) - (2 * constants.minutes))
+
+      clientUtils.sendChat(toClient, constants.midasCornerCommandConfirm(message))
+      return
+    }
+  }
   toServer.write(meta.name, data)
 })
