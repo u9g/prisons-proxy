5561312c075eafcdd20dbaed6369855c7b27f5e5
u9g <43508353+u9g@users.noreply.github.com>
2022-08-28 18:45:17 -0400

    add to auction avg todo

--- auction_utilities.js
+++ auction_utilities.js
@@ -102,6 +102,6 @@ module.exports = {
       }
     }
 
-    // TODO: Slots, Exec Time Extenders, [heroic] meteor flares, cipher
+    // TODO: Slots, Exec Time Extenders, [heroic] meteor flares, cipher, charge orbs
   }
 }
