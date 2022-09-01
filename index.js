1fdc11f3738cab93cd8c284818ef64124578ca41
u9g <43508353+u9g@users.noreply.github.com>
2022-09-01 09:32:48 -0400

    Fix cursor updates being ignored

--- index.js
+++ index.js
@@ -121,13 +121,13 @@ proxy.on('incoming', async (data, meta, toClient, toServer) => {
     else if (type === 2 && meta.name === 'spawn_entity' && config.disable_items_on_ground) return // ore drop animation
   } else if (meta.name === 'set_slot' && data.item.nbtData) {
     // this shouldn't do any harm, but fixes us firing events after the chest has been closed
-    if (!state.inWindow && data.windowId !== 0) return
+    if (!state.inWindow && data.windowId > 0) return
     handleItem(data.item, toClient, toServer)
     modules.forEach(it => it.setSlot(data.item, data.slot, toClient, toServer, config, state))
     if (!state.inWindow && data.slot >= 5 && data.slot <= 8) onArmorSent(data.item, constants.SLOT_TO_ARMOR_NAME[data.slot], toClient, toServer)
   } else if (meta.name === 'window_items') {
     // this shouldn't do any harm, but fixes us firing events after the chest has been closed
-    if (!state.inWindow && data.windowId !== 0) return
+    if (!state.inWindow && data.windowId > 0) return
     let i = 0
     for (const item of data.items) {
       handleItem(item, toClient, toServer)
